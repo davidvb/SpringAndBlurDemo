72e0bbad8228b0c5eccab75be1c7e211df722332
Sergio Campamá <sergiocampama@gmail.com>
2014-01-18 23:08:51 -0300

    Initial Commit

---
+++ SpringAndBlurDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpringAndBlurDemo
+//
+//  Created by Sergio Campamá on 1/18/14.
+//  Copyright (c) 2014 Sergio Campamá. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpringAndBlurDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpringAndBlurDemo
+//
+//  Created by Sergio Campamá on 1/18/14.
+//  Copyright (c) 2014 Sergio Campamá. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
